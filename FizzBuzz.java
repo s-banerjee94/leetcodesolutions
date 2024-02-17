b620be45e7a5be1188ee0f9cd1349db3adeda1ac
Sandeepan Banerjee <shondha.tara94@gmail.com>
2024-02-17 23:55:39 +0530

    Fizz Buzz

---
+++ FizzBuzz.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class FizzBuzz {
+	public static List<String> fizzBuzz(int n) {
+		int p = 1;
+		List<String> reslut = new ArrayList<String>();
+		while (p <= n) {
+			if (p % 3 == 0 && p % 5 == 0)
+				reslut.add("FizzBuzz");
+			else if (p % 3 == 0)
+				reslut.add("Fizz");
+			else if (p % 5 == 0)
+				reslut.add("Buzz");
+			else
+				reslut.add(String.valueOf(p));
+			p++;
+		}
+		return reslut;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(fizzBuzz(5));
+	}
+}
