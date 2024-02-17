6ac050c2c0ebe51573919ebfb60dce1f98a20df1
Sandeepan Banerjee <shondha.tara94@gmail.com>
2024-02-17 23:56:55 +0530

    LinkedList Cycle

---
+++ LinkedListCycle.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+public class LinkedListCycle {
+	public boolean hasCycle(ListNode head) {
+		ListNode pre = head;
+		ListNode left = head;
+		while (pre != null && pre.next != null) {
+			pre = pre.next.next;
+			left = left.next;
+			if (pre == left)
+				return true;
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
