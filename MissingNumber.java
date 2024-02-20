b20b8301e2ec443b98dc4435a7a9e8708d7fdae2
Sandeepan Banerjee <shondha.tara94@gmail.com>
2024-02-20 11:41:38 +0530

    Missing Number

---
+++ MissingNumber.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+public class MissingNumber {
+	public int missingNumber(int[] nums) {
+		int sum = 0;
+		// Finding the sum of all the integers in the array
+		for (int i : nums)
+			sum += i;
+		// The sum of the first n natural numbers is (n * (n + 1)) / 2
+	    // The missing number is the difference between the sum of the first n natural numbers and the sum of the array elements
+	    return ((nums.length * (nums.length + 1)) / 2) - sum;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new MissingNumber().missingNumber(new int[] { 9, 6, 4, 2, 3, 5, 7, 0, 1 }));
+	}
+
+}
