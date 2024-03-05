0715d5759a8ead8e4862cb2bca8296230f6ff6f1
Sandeepan Banerjee <shondha.tara94@gmail.com>
2024-03-05 13:06:12 +0530

    Boats To Save People

---
+++ BoatsToSavePeople.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class BoatsToSavePeople {
+	public int numRescueBoats(int[] people, int limit) {
+		Arrays.sort(people);
+		int left = 0;
+		int right = people.length - 1;
+		int boat = 0;
+		while (left <= right) {
+			if (people[left] + people[right] <= limit)
+				left++;
+			right--;
+			boat++;
+		}
+		return boat;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new BoatsToSavePeople().numRescueBoats(new int[] { 3, 5, 3, 4 }, 5));
+	}
+}
