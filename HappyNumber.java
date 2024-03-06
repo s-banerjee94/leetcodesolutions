22cce04a45d66f42bf86b6f0107286ee8afe5081
Sandeepan Banerjee <shondha.tara94@gmail.com>
2024-03-06 22:52:03 +0530

    Modification in varible names

--- HappyNumber.java
+++ HappyNumber.java
@@ -6,21 +6,23 @@ import java.util.Set;
 public class HappyNumber {
 	public boolean isHappy(int n) {
 		Set<Integer> set = new HashSet<>();
-		int squarSum = n;
-		while(squarSum != 1) {
-			int tempSquarSum = squarSum;
-			squarSum = 0;
-			while(tempSquarSum > 0) {
-				int lastDigit = tempSquarSum % 10;
-				squarSum += Math.multiplyExact(lastDigit, lastDigit);
-				tempSquarSum /= 10;
-			}
-			
-			if(!set.add(squarSum))
-				return false;
-			
-		}
-		return true;
+        int squareSum = n;
+        
+        while (squareSum != 1) {
+            int tempSquareSum = squareSum;
+            squareSum = 0;
+            
+            while (tempSquareSum > 0) {
+                int lastDigit = tempSquareSum % 10;
+                squareSum += lastDigit * lastDigit;
+                tempSquareSum /= 10;
+            }
+            
+            if (!set.add(squareSum))
+                return false;
+        }
+        
+        return true;
 	}
 
 	public static void main(String[] args) {
