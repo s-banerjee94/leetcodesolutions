8a7830cb001d55df48f6bc1ab1f2c5684bbf8de5
Sandeepan Banerjee <shondha.tara94@gmail.com>
2024-03-14 00:57:22 +0530

    Find The Pivot Integer

---
+++ FindThePivotInteger.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+public class FindThePivotInteger {
+	public int pivotInteger(int n) {
+		int start = 1;
+		int end = n;
+		int frontSum = 1;
+		int endSum = n;
+
+		while (start < end) {
+			if (frontSum < endSum) {
+				frontSum += ++start;
+			} else {
+				endSum += --end;
+			}
+		}
+		return frontSum == endSum ? start : -1;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new FindThePivotInteger().pivotInteger(8));
+	}
+
+}
