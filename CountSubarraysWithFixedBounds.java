ea101cf0b5317ab812e3884a6f6d510a74076e45
Sandeepan Banerjee <shondha.tara94@gmail.com>
2024-03-31 22:28:45 +0530

    Count Subarrays With Fixed Bounds

---
+++ CountSubarraysWithFixedBounds.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+public class CountSubarraysWithFixedBounds {
+	public long countSubarrays(int[] nums, int minK, int maxK) {
+		int minIndex = -1;
+		int maxIndex = -1;
+		int culpritIndex = -1;
+		int ans = 0;
+		int smallIndex = -1;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == minK)
+				minIndex = i;
+			if (nums[i] == maxK)
+				maxIndex = i;
+			if (nums[i] > maxK || nums[i] < minK)
+				culpritIndex = i;
+			smallIndex = Math.min(minIndex, maxIndex);
+			int temp = smallIndex - culpritIndex;
+			ans += temp <= 0 ? 0 : temp;
+		}
+		return ans;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new CountSubarraysWithFixedBounds().countSubarrays(new int[] { 1,1,1,1 }, 1, 1));
+	}
+
+}
