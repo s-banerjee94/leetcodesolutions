24bd1c08a0e5022767c7fe8e870d01c1180fcc3f
Sandeepan Banerjee <shondha.tara94@gmail.com>
2024-04-14 22:35:41 +0530

    Sum Of Left Leaves

---
+++ SumOfLeftLeaves.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+public class SumOfLeftLeaves {
+	public int sumOfLeftLeaves(TreeNode root) {
+		return dfs(root, root);
+	}
+
+	private int dfs(TreeNode current, TreeNode root) {
+		if (current == null)
+			return 0;
+		if (current.left == null && current.right == null &&  (root != null && root.left == current)) {
+				return current.val;
+			
+		}
+		int right = dfs(current.right, current);
+		int left = dfs(current.left, current);
+		return left + right;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
