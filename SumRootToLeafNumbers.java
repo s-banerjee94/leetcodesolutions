2fa7c4ca10d6c5084d81586902b7c5fe8aebf88f
Sandeepan Banerjee <shondha.tara94@gmail.com>
2024-04-15 21:49:05 +0530

    Sum Root To Leaf Numbers

---
+++ SumRootToLeafNumbers.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+public class SumRootToLeafNumbers {
+	public int sumNumbers(TreeNode root) {
+		return dfs(root, 0);
+	}
+
+	private int dfs(TreeNode root, int current) {
+		if (root == null)
+			return 0;
+		current = current * 10 + root.val;
+		if(root.left == null && root.right == null) 
+			return current;
+		
+		int left = dfs(root.left, current);
+		int right = dfs(root.right, current);
+
+		return left + right;
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
