ff907d7d91f79b8dabb089d3fc4c1ec0179204b2
s-banerjee94 <shondha.tara94@gmail.com>
2025-07-25 23:57:06 +0530

    add BinarySearch class with iterative binary search implementation

---
+++ BinarySearch.java
@@ -0,0 +1,23 @@
+public class BinarySearch {
+    public static int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if(nums[mid] == target) {
+                return mid;
+            } else if(target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(search(new int[] { 1, 2, 3, 4, 5 }, 3)); // Output: 2
+    }
+}
