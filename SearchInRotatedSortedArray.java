68fb2ea91202334f441150005071ef1b6f1edf52
s-banerjee94 <shondha.tara94@gmail.com>
2025-07-27 13:26:33 +0530

    add SearchInRotatedSortedArray class with binary search solution to find target in rotated array

---
+++ SearchInRotatedSortedArray.java
@@ -0,0 +1,34 @@
+public class SearchInRotatedSortedArray {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            if(nums[mid] == target)
+                return mid;
+            if(nums[mid] < nums[right]) {
+                if(nums[mid] <=  target && nums[right] >= target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            } else {
+                if(nums[left] <= target && nums[mid] >= target) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        SearchInRotatedSortedArray solution = new SearchInRotatedSortedArray();
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        int target = 0;
+        int result = solution.search(nums, target);
+        System.out.println("Target " + target + " found at index: " + result);
+    }
+}
