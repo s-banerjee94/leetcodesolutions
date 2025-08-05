8667f9a49f007c3a1852287670f7d6908ecaa0fb
s-banerjee94 <shondha.tara94@gmail.com>
2025-08-06 02:15:56 +0530

    add SymmetricTree class to implement a method for checking if a binary tree is symmetric

---
+++ SymmetricTree.java
@@ -0,0 +1,14 @@
+public class SymmetricTree {
+    public boolean isSymmetric(TreeNode root) {
+        return isMirror(root.left, root.right);
+
+    }
+
+    private boolean isMirror(TreeNode left, TreeNode right) {
+        if(left == null && right == null)
+            return true;
+        if(left == null || right == null || left.val != right.val)
+            return false;
+        return isMirror(left.left, right.right) && isMirror(left.right, right.left);
+    }
+}
