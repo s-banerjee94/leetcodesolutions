8c62c63fe9e99e6bcb4ae24f6a06872458743be7
s-banerjee94 <shondha.tara94@gmail.com>
2025-08-06 13:34:58 +0530

    add InvertBinaryTree class to implement a method for inverting a binary tree

---
+++ InvertBinaryTree.java
@@ -0,0 +1,13 @@
+public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null)
+            return null;
+
+        invertTree(root.left);
+        invertTree(root.right);
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        return root;
+    }
+}
