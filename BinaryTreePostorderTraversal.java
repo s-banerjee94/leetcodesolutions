9a8dc483a9d10e081ce1122aecb8e21590c0236b
s-banerjee94 <shondha.tara94@gmail.com>
2025-08-10 02:03:48 +0530

    add BinaryTreePostorderTraversal class to implement a method for postorder traversal of a binary tree

---
+++ BinaryTreePostorderTraversal.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreePostorderTraversal {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        traverse(root, result);
+        return result;
+    }
+
+    private void traverse(TreeNode root, List<Integer> result) {
+        if(root == null)
+            return;
+        traverse(root.left, result);
+        traverse(root.right, result);
+        result.add(root.val);
+    }
+}
