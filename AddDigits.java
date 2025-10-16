903a33de50cf67fbf0efaff7078a0063ca9493c0
s-banerjee94 <shondha.tara94@gmail.com>
2025-10-17 01:46:57 +0530

    add AddDigits class to implement methods for adding digits and calculating digital root

---
+++ AddDigits.java
@@ -0,0 +1,19 @@
+public class AddDigits {
+    public int addDigits(int num) {
+        while (num > 10) {
+            int sum = 0;
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            num = sum;
+        }
+        return num;
+    }
+
+    public int solution2(int num) {
+        if (num == 0)
+            return 0;
+        return num % 9 == 0 ? 9 : num % 9;
+    }
+}
