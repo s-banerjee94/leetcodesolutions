645ecf3fab723f3e8b1dc265617d6c7a413eb0bb
s-banerjee94 <shondha.tara94@gmail.com>
2025-10-17 01:48:28 +0530

    fix AddDigits class to correctly handle single-digit sums in addDigits method

--- AddDigits.java
+++ AddDigits.java
@@ -1,6 +1,6 @@
 public class AddDigits {
     public int addDigits(int num) {
-        while (num > 10) {
+        while (num >= 10) {
             int sum = 0;
             while (num > 0) {
                 sum += num % 10;
