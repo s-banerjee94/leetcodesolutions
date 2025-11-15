4b17ef03aab9eaf3f6996adb013e02434801f0ec
s-banerjee94 <shondha.tara94@gmail.com>
2025-11-16 00:39:43 +0530

    add RotateList class to implement a method for rotating a linked list to the right by k positions

---
+++ RotateList.java
@@ -0,0 +1,26 @@
+public class RotateList {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null || k == 0) {
+            return head;
+        }
+
+        ListNode oldTail = head;
+        int n;
+        for (n = 1; oldTail.next != null; n++) {
+            oldTail = oldTail.next;
+        }
+
+        oldTail.next = head;
+
+        k = k % n;
+        int stepsToNewHead = n - k;
+        ListNode newTail = head;
+        for (int i = 1; i < stepsToNewHead; i++) {
+            newTail = newTail.next;
+        }
+        ListNode newHead = newTail.next;
+        newTail.next = null;
+
+        return newHead;
+    }
+}
